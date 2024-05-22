287f29013c26f439a305a1455e9e09ac5f00a423
박수정 <zxzx4342@naver.com>
2024-05-22 14:56:16 +0900

    CategoryRow 생성

--- app/src/main/java/com/crystal/movieapp/ui/components/movie/MovieItem.kt
+++ app/src/main/java/com/crystal/movieapp/ui/components/movie/MovieItem.kt
@@ -7,16 +7,21 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.crystal.movieapp.R
 
 private val CARD_WIDTH = 150.dp
 
@@ -44,6 +49,16 @@ fun MovieItem() {
             ),
             verticalAlignment = Alignment.CenterVertically
         ) {
+            Icon(
+                modifier = Modifier
+                    .padding(4.dp)
+                    .size(12.dp),
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_rating),
+                tint = Color.Black.copy(
+                    alpha = 0.5f
+                ),
+                contentDescription = "rating icon"
+            )
             Text(text = "5.0")
         }
     }
